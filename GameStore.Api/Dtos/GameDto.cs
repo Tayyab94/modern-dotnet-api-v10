2e2d877f7d6dc0cec7e2494d285018d2891f36d7
Tayyabvista4 <tayyab@vista4ventures.com>
2025-12-28 03:32:37 +0500

    first commit.

---
+++ GameStore.Api/Dtos/GameDto.cs
@@ -0,0 +1,16 @@
+﻿namespace GameStore.Api.Dtos
+{
+
+    // Dto is the contract between the client and the server it present the share 
+    // aggrement about the data that will be transferred and used by both sides.
+
+    public record GameDto(
+        int Id,string Name, string Genera,
+        decimal Price, DateOnly ReleaseDate
+        );
+
+    public record CreateGameDto(
+     string Name, string Genera,
+    decimal Price, DateOnly ReleaseDate
+    );
+}
